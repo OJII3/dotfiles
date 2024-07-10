5c910b78416277466e1e9bcb061f1a6970781bf4
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-07-10 14:08:54 +0900

    add git aliases

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -3,11 +3,27 @@
 		enable = true;
 		userName = "ojii3";
     userEmail = "84656786+OJII3@users.noreply.github.com";
+    aliases = {
+      c = "commit -m";
+      co = "checkout";
+      s = "status";
+      p = "pull";
+      pu = "push";
+      b = "branch";
+      d = "diff";
+      l = "log";
+      a = "add";
+      pr = "!gh pr"
+      repo = "!gh repo";
+    }
+    extraConfig = {
+      push = { autoSetupRemote = true; }
+    }
 	};
 
 	programs.gh = {
 		enable = true;
-		extensions = with pkgs; [gh-markdown-preview gh-dash];
+		extensions = with pkgs; [gh-markdown-preview gh-dash gh-copilot];
 		settings = {
 			editor = "nvim";
 		};
