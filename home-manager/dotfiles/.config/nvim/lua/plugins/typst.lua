12c76f8475085fe9710d875b1db9ed54d4a5d385
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-07-27 10:54:47 +0900

    remove typst-preview

--- home-manager/dotfiles/.config/nvim/lua/plugins/typst.lua
+++ home-manager/dotfiles/.config/nvim/lua/plugins/typst.lua
@@ -4,13 +4,4 @@ return {
 		lazy = true,
 		ft = { "typst" },
 	},
-	{
-		"chomosuke/typst-preview.nvim",
-		version = "0.1.*",
-		config = true,
-		build = function()
-			require("typst-preview").update()
-		end,
-		ft = { "typst" },
-	},
 }
