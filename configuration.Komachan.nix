6fb6f761f4809c4f835b3e65272fecdfc599f0e5
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-08-08 21:45:16 +0900

    add poewr management

--- configuration.Komachan.nix
+++ configuration.Komachan.nix
@@ -282,6 +282,17 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  services.tlp.enable = true;
+  services.tlp.settings =
+    {
+      START_CHARGE_THRESH_BAT0 = 80;
+      STOP_CHARGE_THRESH_BAT0 = 95;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      ENERGY_PERF_POLICY_ON_AC = "performance";
+      ENERGY_PERF_POLICY_ON_BAT = "powersave";
+    };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
