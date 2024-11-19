404d3ba5968cd82c3772f9935fcf078fa0bd1db8
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-11-19 23:48:04 +0900

    feat: nvidia open seems good

--- configuration.Renchon.nix
+++ configuration.Renchon.nix
@@ -110,7 +110,7 @@
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     prime = {
       sync.enable = true;
@@ -124,7 +124,7 @@
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+    # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   };
 
   # Enable sound with pipewire.
