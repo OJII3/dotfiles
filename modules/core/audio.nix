efa28332fe4491432a0ae91e410df5734cc7fa07
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-02-11 12:35:48 +0900

    fix: format

--- modules/core/audio.nix
+++ modules/core/audio.nix
@@ -10,7 +10,7 @@
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # media-session.enable = true;
   };
 
   programs.noisetorch = {
