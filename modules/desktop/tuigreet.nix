4754aee66af89126fa6c00c5ca1221865277ce2f
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-02-22 18:43:36 +0900

    fix: tuigreet, image name

--- modules/desktop/tuigreet.nix
+++ modules/desktop/tuigreet.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -- time --remember --remember-session --thme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red --cmd uwsm start -- hyprland.desktop";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --user-menu --remember --remember-session --theme border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red --cmd uwsm start -- hyprland.desktop";
         user = "ojii3";
       };
     };
