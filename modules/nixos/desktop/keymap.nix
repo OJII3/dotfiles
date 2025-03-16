e108a96efd1f43245f422dd612f986781a3590b5
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-03-16 18:22:07 +0900

    fix: mac-like keybinds no worky

--- modules/nixos/desktop/keymap.nix
+++ modules/nixos/desktop/keymap.nix
@@ -18,48 +18,50 @@
       { remap."CapsLock" = [ "Ctrl_L" "Esc" ]; }
     ];
     config.keymap = [
-      {
-        name = "Mac Like";
-        remap = {
-          "SUPER-c" = "Ctrl-c";
-          "SUPER-v" = "Ctrl-v";
-          "SUPER-x" = "Ctrl-x";
-          "SUPER-z" = "Ctrl-z";
-          "SUPER-a" = "Ctrl-a";
-          "SUPER-s" = "Ctrl-s";
-          "SUPER-f" = "Ctrl-f";
-          "SUPER-SHIFT-z" = "Ctrl-SHIFT-z";
-        };
-        application = {
-          not = [ "com.mitchellh.ghostty" "kitty" ];
-        };
-      }
-      {
-        name = "Emacs";
-        remap = {
-          "C-b" = "left";
-          "C-f" = "right";
-          "C-p" = "up";
-          "C-n" = "down";
-          "C-a" = "home";
-          "C-e" = "end";
-          "C-h" = "backspace";
-        };
-        application = {
-          not = [ "com.mitchellh.ghostty" "kitty" ];
-        };
-      }
-      {
-        name = "Mac Like in Terminal";
-        remap = {
-          "SUPER-c" = "Ctrl-Shift-c";
-          "SUPER-v" = "Ctrl-Shift-v";
-          "SUPER-a" = "Ctrl-Shift-a";
-        };
-        application = {
-          only = [ "com.mitchellh.ghostty" "kitty" ];
-        };
-      }
+      # {
+      #   name = "Mac Like";
+      #   remap = {
+      #     "SUPER-c" = "Ctrl-c";
+      #     "SUPER-v" = "Ctrl-v";
+      #     "SUPER-x" = "Ctrl-x";
+      #     "SUPER-z" = "Ctrl-z";
+      #     "SUPER-a" = "Ctrl-a";
+      #     "SUPER-s" = "Ctrl-s";
+      #     "SUPER-f" = "Ctrl-f";
+      #     "SUPER-SHIFT-z" = "Ctrl-SHIFT-z";
+      #   };
+      #   application = {
+      #     not = [ "com.mitchellh.ghostty" "kitty" ];
+      #   };
+      # }
+      # {
+      #   name = "Emacs";
+      #   remap = {
+      #     "C-b" = "left";
+      #     "C-f" = "right";
+      #     "C-p" = "up";
+      #     "C-n" = "down";
+      #     "C-a" = "home";
+      #     "C-e" = "end";
+      #     "C-h" = "backspace";
+      #   };
+      #   application = {
+      #     not = [ "com.mitchellh.ghostty" "kitty" ];
+      #   };
+      # }
+      # {
+      #   name = "Mac Like in Terminal";
+      #   remap = {
+      #     "SUPER-c" = "Ctrl-Shift-c";
+      #     "SUPER-v" = "Ctrl-Shift-v";
+      #     "SUPER-a" = "Ctrl-Shift-a";
+      #     "C-p" = "up";
+      #     "C-n" = "down";
+      #   };
+      #   application = {
+      #     only = [ "com.mitchellh.ghostty" "kitty" ];
+      #   };
+      # }
     ];
   };
 }
