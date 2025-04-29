6933023fa25e4cd100c08caa182ba5d19d3b6ecc
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-04-29 22:57:43 +0900

    add: rbw settings

--- modules/home/bitwarden.nix
+++ modules/home/bitwarden.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     bitwarden-desktop
-    pinentry-qt
+    pinentry-qt # for rbw to work
   ];
+  # Third party bitwarden client that supports libsecret
   programs.rbw = {
     enable = true;
+    settings = {
+      email = "ojii3dev@gmail.com";
+      pinentry = pkgs.pinentry-qt;
+    };
   };
 }
