27c648113564a052eedb7beb8c65a4694fa28ad6
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-06 23:45:46 +0900

    remove: rbw

--- modules/home/bitwarden.nix
+++ modules/home/bitwarden.nix
@@ -1,14 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     bitwarden-desktop
-    pinentry-qt # for rbw to work
   ];
-  # Third party bitwarden client that supports libsecret
-  programs.rbw = {
-    enable = true;
-    settings = {
-      email = "ojii3dev@gmail.com";
-      pinentry = pkgs.pinentry-qt;
-    };
-  };
 }
