48abe4bf45440d28a77989bf5c1d2fea9c08333c
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-08 00:19:41 +0900

    update: wezterm config

--- modules/home/terminal/config/wezterm/wezterm.lua
+++ modules/home/terminal/config/wezterm/wezterm.lua
@@ -4,7 +4,8 @@ local wezterm = require("wezterm")
 -- This table will hold the configuration.
 local config = wezterm.config_builder()
 
-config.color_scheme = "Tokyo Night"
+-- config.color_scheme = "Tokyo Night"
+config.color_scheme = "Github Light (Gogh)"
 config.initial_rows = 120
 config.initial_cols = 280
 config.font_size = 11
@@ -23,27 +24,39 @@ config.enable_scroll_bar = true
 -- }
 
 config.background = {
-	-- {
-	-- 	source = {
-	-- 		File = "/home/ojii3/dotfiles/.config/wezterm/kafka_and_silver_wolf.jpg",
-	-- 	},
-	-- 	horizontal_align = "Center",
-	-- 	repeat_x = "NoRepeat",
-	-- 	opacity = 0.6,
-	-- 	hsb = { brightness = 0.06 },
-	-- },
 	{
 		source = {
-			Color = "black",
+			File = "/home/ojii3/.assets/images/honkai_everyone.png",
+		},
+		horizontal_align = "Center",
+		repeat_x = "NoRepeat",
+		opacity = 1,
+		-- hsb = { brightness = 0.06 },
+	},
+	{
+		source = {
+			Gradient = {
+				colors = { "#ffffff88", "#eeeeff33" },
+				orientation = {
+					Linear = { angle = -30.0 },
+				},
+			},
 		},
 		opacity = 1,
+		width = "100%",
+		height = "100%",
 	},
+	-- {
+	-- 	source = {
+	-- 		Color = "white",
+	-- 	},
+	-- 	opacity = 1,
+	-- },
 }
 
-config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = false
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.6
 config.window_frame = {
 	font_size = 10,
 }
