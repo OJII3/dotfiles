c7a88e4bd278256f07fe8ead656fce4653b2d3c1
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-09 21:59:42 +0900

    update: sops config

--- modules/home/sops.nix
+++ modules/home/sops.nix
@@ -1,19 +1,23 @@
-{ inputs, config, username, ... }:
+{ inputs, config, username, pkgs, ... }:
 {
   imports = [
     inputs.sops-nix.homeManagerModules.sops
   ];
 
   sops = {
-    age.keyFile = "home/${username}/.config/sops/age/keys.txt"; # must not have a password
-    age.generateKey = true;
     defaultSopsFile = ../../assets/secrets/secrets.json;
     defaultSopsFormat = "json";
+    gnupg.home = if pkgs.system == "aarch64-darwin" then "/Users/${username}/.gnupg" else "/home/${username}/.gnupg";
+    gnupg.sshKeyPaths = [ ];
     secrets.gemini_api_key = { };
+    secrets.google_search_api_key = { };
+    secrets.google_search_engine_id = { };
   };
 
   programs.zsh.initExtra = ''
     export GEMINI_API_KEY="$(<${config.sops.secrets.gemini_api_key.path})"
+    export GOOGLE_SEARCH_API_KEY="$(<${config.sops.secrets.google_search_api_key.path})"
+    export GOOGLE_SEARCH_ENGINE_ID="$(<${config.sops.secrets.google_search_engine_id.path})"
   '';
 }
 
