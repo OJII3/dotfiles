0f9c5571f29057c5ee255863433e76b96d8fb5a8
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-12 01:32:41 +0900

    update: renew cipher

--- hosts/Cipher/home-manager.nix
+++ hosts/Cipher/home-manager.nix
@@ -1,13 +1,18 @@
 { ... }: {
   imports = [
     ../../modules/home
+    ../../modules/home/desktop
+    ../../modules/home/bitwarden.nix
     ../../modules/home/dev.nix
+    ../../modules/home/cloudflare-warp.nix
     ../../modules/home/direnv.nix
     ../../modules/home/git
-    ../../modules/home/gpg/console.nix
-    ../../modules/home/sops.nix
+    ../../modules/home/gpg/linux-desktop.nix
+    ../../modules/home/kdeconnect.nix
+    ../../modules/home/kdewallet.nix
     ../../modules/home/neovim
     ../../modules/home/network.nix
+    ../../modules/home/sops.nix
     ../../modules/home/terminal
     ../../modules/home/zsh
   ];
---
+++ hosts/Cipher/nixos.nix
@@ -0,0 +1,43 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ inputs, config, pkgs, ... }:
+{
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../modules/nixos/core
+      ../../modules/nixos/core/virtualisation.nix
+      ../../modules/nixos/desktop
+      ../../modules/nixos/desktop/tuigreet.nix
+      ../../modules/nixos/desktop/sunshine.nix
+    ]
+    ++ (with inputs.nixos-hardware.nixosModules; [
+      common-cpu-intel
+      common-pc-ssd
+    ]);
+
+
+  # Bootloader.
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen; # for waydroid
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+  # graphics
+  services.xserver.videoDrivers = [ "intel" ];
+
+  # system packages
+  # environment.systemPackages = with pkgs; [ glxinfo vulkan-tools ];
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
