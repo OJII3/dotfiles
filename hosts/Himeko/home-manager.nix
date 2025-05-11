68520a6b754eb8d3d983972470c87667b292befb
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-11 13:20:27 +0900

    fix: ssh auth sock

--- hosts/Himeko/home-manager.nix
+++ hosts/Himeko/home-manager.nix
@@ -1,16 +1,19 @@
 { ... }: {
   imports = [
     ../../modules/home
-    ../../modules/home/git
-    ../../modules/home/neovim
-    ../../modules/home/zsh
-    ../../modules/home/gpg
-
     ../../modules/home/browser.nix
     ../../modules/home/dev.nix
     ../../modules/home/direnv.nix
+    ../../modules/home/git
+    ../../modules/home/gpg
+    ../../modules/home/neovim
+    ../../modules/home/sops.nix
     ../../modules/home/terminal/config.nix
+    ../../modules/home/zsh
   ];
 
   programs.kitty.enable = true;
+  programs.zsh.initExtra = ''
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  '';
 }
