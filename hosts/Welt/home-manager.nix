808c8baefe42a2366508487ae5e9625c56a3c575
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-14 09:07:52 +0100

    update

--- hosts/Welt/home-manager.nix
+++ hosts/Welt/home-manager.nix
@@ -1,14 +1,17 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ../../modules/home
-    ../../modules/home/neovim
-    ../../modules/home/zsh
-    ../../modules/home/gpg/linux-console.nix
-
     ../../modules/home/dev.nix
     ../../modules/home/direnv.nix
     ../../modules/home/git
+    ../../modules/home/gpg/console.nix
     ../../modules/home/kdewallet.nix
+    ../../modules/home/neovim
     ../../modules/home/network.nix
+    ../../modules/home/zsh
+  ];
+
+  home.packages = with pkgs; [
+    toybox
   ];
 }
