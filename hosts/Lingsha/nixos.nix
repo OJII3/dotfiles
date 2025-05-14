f78da6858f5aa1d812c769c164bea856e8784922
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-14 13:50:43 +0900

    update: fprint

--- hosts/Lingsha/nixos.nix
+++ hosts/Lingsha/nixos.nix
@@ -54,10 +54,6 @@
   };
   services.fprintd = {
     enable = true;
-    tod = {
-      enable = true;
-      driver = pkgs.libfprint-2-tod1-goodix-550a;
-    };
   };
 
   security.pam.services.hyprlock.fprintAuth = true;
