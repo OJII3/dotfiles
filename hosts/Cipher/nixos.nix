0752cb2c361099471ad53f08716bcc2b7b4f4915
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-22 17:26:20 +0900

    cipher: update vmbr

--- hosts/Cipher/nixos.nix
+++ hosts/Cipher/nixos.nix
@@ -57,6 +57,40 @@
     ipAddress = "192.168.0.100";
   };
 
+  networking.bridges.veth0.interfaces = [ "enp3s0" ];
+  networking.interfaces.veth0.ipv4.addresses = [{
+    address = "192.168.20.1";
+    prefixLength = 24;
+  }];
+  networking.nat = {
+    enable = true;
+    externalInterface = "wlp1s0";
+    internalInterfaces = [ "veth0" ];
+    internalIPs = [ "192.168.20.0/24" ];
+  };
+
+  services.kea.dhcp4 = {
+    enable = true;
+    settings = {
+      interfaces-config.interfaces = [ "veth0" ];
+      lease-database = {
+        type = "memfile";
+        persist = true;
+        name = "/var/lib/kea/dhcp4.leases";
+      };
+      subnet4 = [{
+        id = 1;
+        subnet = "192.168.20.0/24";
+        pools = [{ pool = "192.168.20.100 - 192.168.20.200"; }];
+        option-data = [
+          { name = "routers"; data = "192.168.20.1"; }
+          { name = "domain-name-servers"; data = "192.168.20.1,1.1.1.1,8.8.8.8"; }
+        ];
+      }];
+    };
+  };
+
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
