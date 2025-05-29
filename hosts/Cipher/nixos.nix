d4809197a94f266611ebdd596b2d3cd677bead83
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-05-30 04:19:21 +0900

    cipher: network setup done

--- hosts/Cipher/nixos.nix
+++ hosts/Cipher/nixos.nix
@@ -2,15 +2,16 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ inputs, lib, pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports =
     [
       # ../../modules/nixos/core/k3s.nix
       ../../modules/nixos/core
+      ../../modules/nixos/core/boot/systemd-boot.nix
+      ../../modules/nixos/core/k3s/server.nix
       ../../modules/nixos/core/networking
       ../../modules/nixos/core/proxmox.nix
-      ../../modules/nixos/core/boot/systemd-boot.nix
 
       ../../modules/nixos/desktop
       ../../modules/nixos/desktop/greetd/autologin.nix
@@ -30,10 +31,12 @@
   # graphics
   services.xserver.videoDrivers = [ "intel" ];
 
-  # networkd
-  # networking.bridges.vmbr0.interfaces = [ "enp3s0" ];
-  # networking.interfaces.vmmbr0.useDHCP = lib.mkDefault true;
+  # proxomox-ve host ip
+  services.proxmox-ve = {
+    ipAddress = "192.168.0.100";
+  };
 
+  # usual network settings
   networking.useNetworkd = true;
   networking.networkmanager.enable = false;
   networking.interfaces."wlp1s0" = {
@@ -49,27 +52,53 @@
   networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
   networking = {
     wireless.enable = true;
+    wireless.secretsFile = "/run/secrets/wireless.conf";
     wireless.networks."aterm-44cbf4-a" = {
-      pskRaw = "f28bf2655884695d0ea0948f78334b2885c9f13152d7f9af11b98ca134ea4d54";
+      pskRaw = "ext:psk_home";
     };
   };
-  services.proxmox-ve = {
-    ipAddress = "192.168.0.100";
+
+  # create bridge for mini dhcp server
+  systemd.network.netdevs."br0".netdevConfig = {
+    Name = "br0";
+    Kind = "bridge";
+  };
+  systemd.network.networks."00-br0" = {
+    matchConfig.Name = "br0";
+    networkConfig = {
+      Address = "10.42.0.1/24";
+      DHCPServer = "yes";
+    };
+    dhcpServerConfig = {
+      PoolOffset = 100;
+      PoolSize = 100;
+    };
   };
 
-  networking.bridges.veth0.interfaces = [ "enp3s0" ];
-  networking.interfaces.veth0.ipv4.addresses = [{
-    address = "192.168.20.1";
-    prefixLength = 24;
-  }];
+  # enp3s0 to br0
+  systemd.network.networks."20-enp3s0" = {
+    matchConfig.Name = "enp3s0";
+    networkConfig = { Bridge = "br0"; };
+  };
+
+  # vm taps to br0
+  systemd.network.networks."30-tap0" = {
+    matchConfig.Name = "tap*";
+    networkConfig = { Bridge = "br0"; };
+  };
+
+  # nat via wlp1s0
   networking.nat = {
     enable = true;
     externalInterface = "wlp1s0";
-    internalInterfaces = [ "veth0" ];
-    internalIPs = [ "192.168.20.0/24" ];
+    internalInterfaces = [ "br0" ];
   };
 
+  # open firewall for dhcp and dns
+  networking.firewall.allowedUDPPorts = [ 53 67 ];
+  networking.firewall.allowedTCPPorts = [ 53 ];
 
+  # samba
   services.samba = {
     enable = true;
     securityType = "user";
@@ -80,47 +109,27 @@
         "server string" = "cipher";
         "netbios name" = "cipher";
         "security" = "user";
-        #"use sendfile" = "yes";
+        "use sendfile" = "yes";
         #"max protocol" = "smb2";
         # note: localhost is the ipv6 localhost ::1
-        "hosts allow" = "0.0.0.0";
+        "hosts allow" = "*";
         # "hosts deny" = "0.0.0.0/0";
         "guest account" = "nobody";
         "map to guest" = "bad user";
       };
-      "private" = {
-        "path" = "/mnt/Shared/Private";
+      "Private" = {
+        "path" = "/home/ojii3/Shared";
         "browseable" = "yes";
         "read only" = "no";
         "guest ok" = "no";
         "create mask" = "0644";
         "directory mask" = "0755";
         "force user" = "ojii3";
-        "force group" = "WORKGROUP";
+        # "force group" = "WORKGROUP";
       };
     };
   };
 
-  services.kea.dhcp4 = {
-    enable = true;
-    settings = {
-      interfaces-config.interfaces = [ "veth0" ];
-      lease-database = {
-        type = "memfile";
-        persist = true;
-        name = "/var/lib/kea/dhcp4.leases";
-      };
-      subnet4 = [{
-        id = 1;
-        subnet = "192.168.20.0/24";
-        pools = [{ pool = "192.168.20.100 - 192.168.20.200"; }];
-        option-data = [
-          { name = "routers"; data = "192.168.20.1"; }
-          { name = "domain-name-servers"; data = "192.168.20.1,1.1.1.1,8.8.8.8"; }
-        ];
-      }];
-    };
-  };
 
 
   # This value determines the NixOS release from which the default
