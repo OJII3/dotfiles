cf61dcf0704c5c1d412a1d59d530b14c3b4002b4
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-06-11 22:29:24 +0900

    nix: update flake

--- modules/nixos/core/proxmox.nix
+++ modules/nixos/core/proxmox.nix
@@ -7,5 +7,14 @@
     enable = true;
     # ipAddress = "192.168.0.100";
   };
+
+  nix.settings = {
+    substituters = [
+      "https://cache.saumon.network/proxmox-nixos"
+    ];
+    trusted-public-keys = [
+      "proxmox-nixos:D9RYSWpQQC/msZUWphOY2I5RLH5Dd6yQcaHIuug7dWM="
+    ];
+  };
 }
 
