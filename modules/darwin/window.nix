4f42ebe44e296a429a676c1d4b425bab8bf6a6ab
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-06-24 11:32:00 +0900

    himeko: fix border

--- modules/darwin/window.nix
+++ modules/darwin/window.nix
@@ -1,7 +1,7 @@
 {
   services.jankyborders = {
     enable = true;
-    active_color = "gradient(top_left=0xFFFFA500,bottom_right=0xFF1C6293)";
+    active_color = "0xFFFFA500";
     inactive_color = "0x00000000";
   };
 
