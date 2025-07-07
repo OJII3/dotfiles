8434484fa2064054b137fd0e1af02aeb65dfb70e
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-07-07 13:20:28 +0900

    home: fix mise global settings

--- modules/home/dev/mise.nix
+++ modules/home/dev/mise.nix
@@ -9,6 +9,9 @@
       alias = {
         node = "https://github.com/asdf-vm/asdf-nodejs";
       };
+      settings = {
+        idiomatic_version_file_enable_tools = [ "node" ];
+      };
     };
   };
 }
