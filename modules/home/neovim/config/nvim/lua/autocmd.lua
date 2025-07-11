6f43c647a5ed180ae43895b64e0f8200976304e1
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-07-11 10:49:10 +0900

    home: add neovim hook disable copilot in procon

--- modules/home/neovim/config/nvim/lua/autocmd.lua
+++ modules/home/neovim/config/nvim/lua/autocmd.lua
@@ -12,3 +12,13 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
 	pattern = "*",
 	command = "startinsert",
 })
+
+-- when pwd is ~/src/github.com/OJII3/procon (case insensitive), run :Copilot disable
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	pattern = "*",
+	callback = function()
+		if string.match(vim.fn.getcwd(), "github.com/OJII3/procon", 1) then
+			vim.cmd("Copilot disable")
+		end
+	end,
+})
