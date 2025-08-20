34f9a2cd16ff068079a66ad3dc7814e97476ce83
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-08-20 15:43:35 +0900

    feat: upddaate swaync

--- modules/home/desktop/swaync/default.nix
+++ modules/home/desktop/swaync/default.nix
@@ -2,6 +2,15 @@
   services.swaync = {
     enable = true;
     settings = builtins.fromJSON ''${builtins.readFile ./config.json}'';
-    style = builtins.readFile ./style.css;
+    style = ''
+      ${builtins.readFile ./style.scss}
+      ${builtins.readFile ./widgets/buttons.scss}
+      ${builtins.readFile ./widgets/label.scss}
+      ${builtins.readFile ./widgets/menubar.scss}
+      ${builtins.readFile ./widgets/mpris.scss}
+      ${builtins.readFile ./widgets/slider.scss}
+      ${builtins.readFile ./widgets/title.scss}
+      ${builtins.readFile ./widgets/volume.scss}
+    '';
   };
 }
