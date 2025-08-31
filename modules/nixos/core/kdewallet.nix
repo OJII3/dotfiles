3ff6903a29b6bbbf61b96308f0b1a26e96209106
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-08-31 15:18:16 +0900

    fix: headless kwallet

--- modules/nixos/core/kdewallet.nix
+++ modules/nixos/core/kdewallet.nix
@@ -1,7 +1,12 @@
-{ ... }: {
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    kdePackages.kwallet
+    kdePackages.kwallet-pam
+  ];
   # Kwallet for no GUI sessions
   security.pam.services.login.kwallet = {
     enable = true;
     forceRun = true;
   };
 }
+
