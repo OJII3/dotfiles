d3899c028cbb572af37884c5a32541be65ec1345
OJII3 <84656786+OJII3@users.noreply.github.com>
2025-09-23 18:26:58 +0900

    remove git command aliases and adjust PSFzf options in PowerShell profile

--- windows/powershell/profile.ps1
+++ windows/powershell/profile.ps1
@@ -15,20 +15,8 @@ Import-Module -Name Microsoft.WinGet.CommandNotFound
 #f45873b3-b655-43a6-b217-97c00aa0db58
 
 Import-Module Abbr
-
 ealias g 'git'
 
-ealias 'git a' 'git add'
-ealias 'git b' 'git branch'
-ealias 'git c' 'git commit -m'
-ealias 'git f' 'git fetch'
-ealias 'git fetch p' 'git fetch --prune'
-ealias 'git h' 'git push'
-ealias 'git p' 'git pull'
-ealias 'git s' 'git status'
-ealias 'git switch c' 'git switch -c'
-ealias 'git w' 'git switch'
-
-ealias 'gh p' 'gh pr'
-ealias 'gh pr c' 'gh pr create'
-ealias 'gh pr m' 'gh pr merge'
+# Install-Module -Name PSFzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r'
