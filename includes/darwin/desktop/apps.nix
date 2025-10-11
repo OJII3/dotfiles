26dd89a83009bb6257052dffcb6405bfed45f089
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-10-12 00:22:50 +0900

    ghostty: update fonts

--- includes/darwin/desktop/apps.nix
+++ includes/darwin/desktop/apps.nix
@@ -2,11 +2,15 @@
   imports = [ ./. ];
   homebrew = {
     enable = true;
-    onActivation.upgrade = true;
-    onActivation.autoUpdate = false;
+    onActivation = {
+      upgrade = true;
+      autoUpdate = false;
+      cleanup = "uninstall";
+    };
     casks = [
       "chatgpt"
       "firefox"
+      "fork"
       "ghostty"
       "logi-options+"
       "ltspice"
