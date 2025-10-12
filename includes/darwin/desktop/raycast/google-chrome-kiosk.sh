6c3e42c180cca99366fc0bac7af8d6f329d4afca
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-10-12 13:32:19 +0900

    darwin: add raycast command

---
+++ includes/darwin/desktop/raycast/google-chrome-kiosk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Google Chrome Kiosk
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🌙
+# @raycast.packageName Google Chrome Kiosk
+
+# Documentation:
+# @raycast.description Google Chrome Profile Picker
+# @raycast.author ojii3dev
+# @raycast.authorURL https://raycast.com/ojii3dev
+
+open -na "Google Chrome" --args --kiosk
