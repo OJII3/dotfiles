75d77c42c056817c4a053a902ec2319d4585fa77
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-10-15 14:59:32 +0900

    macos: add vr

--- hosts/Himeko/darwin.nix
+++ hosts/Himeko/darwin.nix
@@ -1,10 +1,11 @@
 { pkgs, inputs, ... }:
 {
   imports = [
-    ../../includes/darwin/core/sops.nix
     ../../includes/darwin/core/fonts.nix
     ../../includes/darwin/core/networking.nix
+    ../../includes/darwin/core/sops.nix
     ../../includes/darwin/desktop/apps.nix
+    ../../includes/darwin/desktop/vr.nix
   ];
 
   # List packages installed in system profile. To search by name, run:
