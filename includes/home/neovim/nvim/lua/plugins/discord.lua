5a576e69f6fef3cc9bbe5db77303c319af6dd6f6
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-11-23 13:32:07 +0900

    fix: presence.nvim config

--- includes/home/neovim/nvim/lua/plugins/discord.lua
+++ includes/home/neovim/nvim/lua/plugins/discord.lua
@@ -2,6 +2,10 @@ return {
 	"andweeb/presence.nvim",
 	opts = {
 		neovim_image_text = "ねお♡びむ",
+		editing_text = "Editing Text...",
+		file_explorer_text = "Browsing Files...",
+		reading_text = "Reading Documents...",
+		workspace_text = "Working on Projects...",
 	},
 	event = "BufReadPost",
 }
