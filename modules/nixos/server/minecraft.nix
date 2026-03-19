5c36d2bf1117396d64682b7a2d919630ee749785
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-03-19 23:14:36 +0900

    feat: update flake

--- modules/nixos/server/minecraft.nix
+++ modules/nixos/server/minecraft.nix
@@ -7,6 +7,14 @@
 }:
 let
   cfg = config.dot.server.minecraft;
+  opsFile = pkgs.writeText "ops.json" (builtins.toJSON [
+    {
+      uuid = "00d75b68-b321-4484-a8ba-2f52ac4fc551";
+      name = "ojii3dev";
+      level = 4;
+      bypassesPlayerLimit = true;
+    }
+  ]);
 in
 {
   options.dot.server.minecraft = {
@@ -29,5 +37,9 @@ in
       };
       jvmOpts = "-Xmx2G -Xms1G";
     };
+
+    systemd.services.minecraft-server.preStart = lib.mkAfter ''
+      ln -sf ${opsFile} ${config.services.minecraft-server.dataDir}/ops.json
+    '';
   };
 }
