445a9cb63c280ea9cee5f41bd1089d35617ed8b7
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-06-09 19:10:50 +0900

    feat: add trusted pub keys

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -151,6 +151,10 @@ in
             trusted-substituters = [
               "https://ros.cachix.org"
             ];
+            trusted-public-keys = [
+              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+              "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
+            ];
           };
           gc = {
             automatic = true;
