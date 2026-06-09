4f9e3f67733032cd7fbc75996d15be3f44b632fa
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-06-09 19:20:19 +0900

    feat: update kdeconnect configs

--- modules/nixos/networking/options.nix
+++ modules/nixos/networking/options.nix
@@ -16,7 +16,7 @@
       };
 
       ros2 = {
-        enable = lib.mkEnableOption "ROS 2 UDP ports (all)" // {
+        enable = lib.mkEnableOption "Open ROS 2 UDP ports (all)" // {
           default = false; # Security: disabled by default
         };
       };
