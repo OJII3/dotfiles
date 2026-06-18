b23156972c2900f62176e302f35089a42cef2356
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-06-18 17:59:41 +0900

    feat: add nixfmt-tree formatter and formatting check

--- flake.nix
+++ flake.nix
@@ -47,10 +47,28 @@
     codex-desktop-linux.url = "github:ilysenko/codex-desktop-linux";
   };
 
-  outputs = inputs: {
+  outputs = inputs: let
+    inherit (inputs.nixpkgs) lib;
+    systems = lib.systems.flakeExposed;
+  in {
     nixosConfigurations = (import ./hosts inputs).nixos;
     homeConfigurations = (import ./hosts inputs).home-manager;
     darwinConfigurations = (import ./hosts inputs).nix-darwin;
     nixOnDroidConfigurations = (import ./hosts inputs).nix-on-droid;
+
+    formatter = lib.genAttrs systems (system:
+      inputs.nixpkgs.legacyPackages.${system}.nixfmt-tree
+    );
+
+    checks = lib.genAttrs systems (system: {
+      formatting = inputs.nixpkgs.legacyPackages.${system}.runCommand
+        "nix-fmt-check-${system}"
+        { src = ./.; }
+        ''
+          cd $src
+          ${inputs.nixpkgs.legacyPackages.${system}.nixfmt-tree}/bin/treefmt --ci
+          touch $out
+        '';
+    });
   };
 }
