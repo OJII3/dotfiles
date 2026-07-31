61a3999e5233aa6cf9ff21e6ccb361f33710f51b
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-08-01 03:24:59 +0900

    feat: add search domains to macos

--- modules/darwin/networking/base.nix
+++ modules/darwin/networking/base.nix
@@ -1,13 +1,22 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  hostname,
+  ...
+}:
 let
   cfg = config.dot.darwin.networking;
 in
 {
   config = lib.mkIf cfg.enable {
-    networking.knownNetworkServices = [
-      "USB 10/100/1000 LAN"
-      "Thunderbolt Bridge"
-      "Wi-Fi"
-    ];
+    networking = {
+      search = [ "internal.ojii3.dev" ];
+      hostName = hostname;
+      knownNetworkServices = [
+        "USB 10/100/1000 LAN"
+        "Thunderbolt Bridge"
+        "Wi-Fi"
+      ];
+    };
   };
 }
