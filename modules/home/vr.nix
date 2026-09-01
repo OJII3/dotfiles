58df4e3718b5c0cdc437417868d8de003a3b4de4
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-09-01 13:15:09 +0900

    chore: format

--- modules/home/vr.nix
+++ modules/home/vr.nix
@@ -8,26 +8,25 @@ let
   cfg = config.dot.home;
 in
 {
-  config =
-    lib.mkIf cfg.vr.enable {
-      xdg.configFile."openvr/openvrpaths.vrpath".text = ''
-        {
-          "config" :
-          [
-            "~/.local/share/Steam/config"
-          ],
-          "external_drivers" : null,
-          "jsonid" : "vrpathreg",
-          "log" :
-          [
-            "~/.local/share/Steam/logs"
-          ],
-          "runtime" :
-          [
-            "${pkgs.opencomposite}/lib/opencomposite"
-          ],
-          "version" : 1
-        }
-      '';
-    };
+  config = lib.mkIf cfg.vr.enable {
+    xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+      {
+        "config" :
+        [
+          "~/.local/share/Steam/config"
+        ],
+        "external_drivers" : null,
+        "jsonid" : "vrpathreg",
+        "log" :
+        [
+          "~/.local/share/Steam/logs"
+        ],
+        "runtime" :
+        [
+          "${pkgs.opencomposite}/lib/opencomposite"
+        ],
+        "version" : 1
+      }
+    '';
+  };
 }
