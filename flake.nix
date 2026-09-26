f5bb7368e406a1c71df9c0f4d482a1f200587c99
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-09-27 03:28:23 +0900

    fix: accept flake config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   nixConfig = {
+    accept-flake-config = true;
     extra-substituters = [ "https://cache.numtide.com" ];
     extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
   };
